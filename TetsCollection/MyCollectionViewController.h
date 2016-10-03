1c2da345243a83b7ecd8978cd1d55e69cb1e696b
Boopslo <oslophantom@gmail.com>
2016-10-03 13:01:03 +0800

    collectionView layout alike a calendar

--- TetsCollection/MyCollectionViewController.h
+++ TetsCollection/MyCollectionViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MyCollectionViewController.h
 //  TetsCollection
 //
 //  Created by Oslo on 10/3/16.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface MyCollectionViewController : UICollectionViewController
 
 @end
-
